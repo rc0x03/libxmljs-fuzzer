38929fe9a7fe21499ccff5ef350181093bf18c34
rc0x03 <chipka01@email.franklin.edu>
2015-12-12 16:17:05 -0600

    Added set_text.js

--- ops/set_text.js
+++ ops/set_text.js
@@ -1,5 +1,6 @@
 module.exports = function(libxmljs, docs, nodes, log) {
     var node = nodes.random();
     if (!node || node.type() !== 'element') return false;
+    log(node.name());
     node.text('');
 }
